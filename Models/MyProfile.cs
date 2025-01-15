10a7abf8e07da3c39053903a38b454557a0516e4
Shienny Mendeline <43661487+shiennymendeline@users.noreply.github.com>
2025-01-15 23:07:45 +0700

    added json file
    modified the values

---
+++ Models/MyProfile.cs
@@ -0,0 +1,57 @@
+﻿namespace shiennymendeline.github.io.Models
+{
+    public class MyProfile
+    {
+        public Profile Profile { get; set; }
+    }
+
+    public class Profile
+    {
+        public string Greeting1 { get; set; }
+        public string Greeting2 { get; set; }
+        public string Summary { get; set; }
+        public string ProfileImg { get; set; }
+        public string GotoLink { get; set; }
+        public List<LinkInfo> Links { get; set; }
+    }
+
+    public class AboutMe
+    {
+        public string Title { get; set; }
+        public string AboutmeImg { get; set; }
+        public string Description { get; set; }
+        public List<Highlight> highlights { get; set; }
+        public string GotoLink { get; set; }
+    }
+
+    public class Skill
+    {
+        public string Title { get; set; }
+        public string PlaceholderSearch { get; set; }
+        public string TextSearch { get; set; }
+        public string AllInfo { get; set; }
+        public List<ItemOption> Categories { get; set; }
+        public List<SkillItem> Skills { get; set; }
+    }
+
+    public class SkillItem
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Level { get; set; }
+        public string IconImg { get; set; }
+    }
+
+    public class LinkInfo
+    {
+        public string LinkName { get; set; }
+        public string Link { get; set; }
+    }
+
+    public class Highlight
+    {
+        public string Title { get; set; }
+        public string Caption { get; set; }
+        public string IconImg { get; set; }
+    }
+}
--- Pages/Home.razor.cs
+++ Pages/Home.razor.cs
@@ -2,13 +2,16 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using shiennymendeline.github.io.Models;
 using shiennymendeline.github.io.Services;
+using System.Net.Http.Json;
+using static shiennymendeline.github.io.Pages.Weather;
+using static System.Net.WebRequestMethods;
 
 namespace shiennymendeline.github.io.Pages
 {
     public partial class Home : ComponentBase
     {
         [Inject] IJSService JSService { get; set; } = default!;
-
+        [Inject] HttpClient Http { get; set; } = default!;
         public string searchSkill { get; set; } = "";
         public List<string> selectedSkillsForProject { get; set; } = new List<string>();
         public List<ItemOption> skillCategories { get; set; } = new()
@@ -33,6 +36,11 @@ namespace shiennymendeline.github.io.Pages
                 ButtonSecondaryLink = "#"
             }
         };
+
+        protected async override Task OnInitializedAsync()
+        {
+            await Http.GetFromJsonAsync<WeatherForecast[]>("data/profileinfo.json");
+        }
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
