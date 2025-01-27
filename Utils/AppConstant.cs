5f97086fbcc636907603be0b36c99573bf926400
Shienny Mendeline <43661487+shiennymendeline@users.noreply.github.com>
2025-01-28 00:35:23 +0700

    remove unnecessary files

--- Utils/AppConstant.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace shiennymendeline.github.io.Utils
-{
-    public static class AppConstant
-    {
-        public static readonly string VerifiedImgPath = "images/verify.png";
-        public static readonly string MsgNoSelected = "Nothing selected";
-    }
-}
